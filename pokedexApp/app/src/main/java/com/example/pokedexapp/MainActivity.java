8f26ec20bdfffdef99fdcb823f243d94e4e34974
Galactius <dperez6@villanova.edu>
2021-11-30 12:38:28 -0500

    started data procesing in main

--- pokedexApp/app/src/main/java/com/example/pokedexapp/MainActivity.java
+++ pokedexApp/app/src/main/java/com/example/pokedexapp/MainActivity.java
@@ -4,12 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity
+{
+    private final LinkedList<String> mPokemonNameList = new LinkedList<>();
+    private final LinkedList<String> mPokemonTypeList = new LinkedList<>();
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is a comment
+
+        processData();
     }
+
+    protected void processData()
+    {
+        int numOfPKMNs = 10; //number of pokemons to process
+        for(int curPKMN = 0; curPKMN < numOfPKMNs; curPKMN++)
+        {
+            //mPokemonNameList.add( INSERT PKMN NAME FROM API);
+            //mPokemonTypeList.add( INSERT PKMN TYPES FROM API AS CONCAT STR);
+        }
+    }
+
 }
