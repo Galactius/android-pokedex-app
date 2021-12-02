b324f6e763f3e3e2a7fbdf7cb38a809421f782bc
Galactius <dperez6@villanova.edu>
2021-12-02 11:52:54 -0500

    added adapter file & updated mainActivity

--- pokedexApp/app/src/main/java/com/example/pokedexapp/MainActivity.java
+++ pokedexApp/app/src/main/java/com/example/pokedexapp/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.pokedexapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity
 {
+    private final LinkedList<Integer> mPokemonNumberList = new LinkedList<>();
     private final LinkedList<String> mPokemonNameList = new LinkedList<>();
     private final LinkedList<String> mPokemonTypeList = new LinkedList<>();
+    private final LinkedList<String> mPokemonAbilityList = new LinkedList<>();
+    private final LinkedList<String> mPokemonDescList = new LinkedList<>();
+    private final LinkedList<ImageView> mPokemonImageList = new LinkedList<>();
 
-
+    private int numOfPKMNs = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,7 +29,7 @@ public class MainActivity extends AppCompatActivity
 
     protected void processData()
     {
-        int numOfPKMNs = 10; //number of pokemons to process
+        numOfPKMNs = 10; //number of pokemons to process
         for(int curPKMN = 0; curPKMN < numOfPKMNs; curPKMN++)
         {
             //mPokemonNameList.add( INSERT PKMN NAME FROM API);
---
+++ pokedexApp/app/src/main/java/com/example/pokedexapp/pokedexAdapter.java
@@ -0,0 +1,4 @@
+package com.example.pokedexapp;
+
+public class pokedexAdapter {
+}
