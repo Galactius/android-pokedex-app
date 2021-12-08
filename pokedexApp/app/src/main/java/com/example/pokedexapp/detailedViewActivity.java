33da63219121e7aebbe860d86bfaf6362304de96
Galactius <dperez6@villanova.edu>
2021-12-08 11:15:19 -0500

    implemented planned changes to second activity

--- pokedexApp/app/src/main/java/com/example/pokedexapp/detailedViewActivity.java
+++ pokedexApp/app/src/main/java/com/example/pokedexapp/detailedViewActivity.java
@@ -2,13 +2,37 @@ package com.example.pokedexapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class detailedViewActivity extends AppCompatActivity {
+public class detailedViewActivity extends AppCompatActivity
+{
+    public TextView mType;
+    public TextView mDesc;
+    public TextView mAbility;
+    public ImageView mImage;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_view);
+
+//         mType = findViewById(R.id.typeText);
+//         mDesc = findViewById(R.id.descText);
+//         mAbility = findViewById(R.id.abilText);
+//         mImage = findViewById(R.id.img);
+//         
+         Intent intent = getIntent();
+         mType.setText(intent.getStringExtra("type"));
+         mDesc.setText(intent.getStringExtra("desc"));
+         mAbility.setText(intent.getStringExtra("abil"));
+
+         //mImage.setImageResource(...);
+
+
+
     }
 }
