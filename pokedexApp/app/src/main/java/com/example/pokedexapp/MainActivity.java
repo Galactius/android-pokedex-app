0cd10041529524061763e3c85c85ee022f63be8a
NateTheCat <ndiazsan@villanova.edu>
2021-12-09 00:27:44 -0500

    changed  numlist type to string

--- pokedexApp/app/src/main/java/com/example/pokedexapp/MainActivity.java
+++ pokedexApp/app/src/main/java/com/example/pokedexapp/MainActivity.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,14 +18,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.ContentHandler;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity
 {
-    private final LinkedList<Integer> mPokemonNumberList = new LinkedList<>();
+    private final LinkedList<String> mPokemonNumberList = new LinkedList<String>();
     private final LinkedList<String> mPokemonNameList = new LinkedList<>();
     private final LinkedList<String> mPokemonTypeList = new LinkedList<>();
     private final LinkedList<String> mPokemonAbilityList = new LinkedList<>();
@@ -72,6 +69,8 @@ public class MainActivity extends AppCompatActivity
 
         mPokemonNameList.add(name);
         Log.d("ListAddTest", mPokemonNameList.get(0));
+
+        mPokemonNumberList.add("1");
 
 
     }
